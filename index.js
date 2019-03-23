eead5a318ad7124f7b54e7d1cfe66dcad674aa67
var-bin <var-bin@users.noreply.github.com>
2019-03-23 17:21:50 +0200

    Fix eslint issues
    Signed-off-by: var-bin <var-bin@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
 import App from './src/App';
+
 const root = document.getElementById('app');
 
 render(<App />, root);
