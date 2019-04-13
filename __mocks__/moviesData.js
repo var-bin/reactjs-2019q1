1f7c4d9d55cf43684a6dfb13103b98a60a9b3a66
var-bin <var-bin@users.noreply.github.com>
2019-04-13 19:01:09 +0300

    Refactoring: Replace mocked data for movies to __mocks__ directory

---
+++ __mocks__/moviesData.js
@@ -0,0 +1,18 @@
+export const moviesData = [{
+  id: 348350,
+  title: 'Solo: A Star Wars Story',
+  tagline: '',
+  vote_average: 0,
+  vote_count: 2,
+  release_date: '2018-05-23',
+  poster_path: 'https://image.tmdb.org/t/p/w500/uJ6OnE3CzGWq6buLINAbdBqa0gV.jpg',
+  overview: 'Through a series of daring escapades deep within a dark and dangerous criminal underworld, Han Solo meets his mighty future copilot Chewbacca and encounters the notorious gambler Lando Calrissian.',
+  budget: 0,
+  revenue: 0,
+  genres: [
+    'Action',
+    'Adventure',
+    'Family',
+    'Science Fiction'
+  ]
+}];
--- src/components/movies/movies.test.jsx
+++ src/components/movies/movies.test.jsx
@@ -1,34 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { MovieTile } from '../movie-tile';
 import { Movies } from './movies';
-
-const mockedMoviesData = [{
-  id: 348350,
-  title: 'Solo: A Star Wars Story',
-  tagline: '',
-  vote_average: 0,
-  vote_count: 2,
-  release_date: '2018-05-23',
-  poster_path: 'https://image.tmdb.org/t/p/w500/uJ6OnE3CzGWq6buLINAbdBqa0gV.jpg',
-  overview: 'Through a series of daring escapades deep within a dark and dangerous criminal underworld, Han Solo meets his mighty future copilot Chewbacca and encounters the notorious gambler Lando Calrissian.',
-  budget: 0,
-  revenue: 0,
-  genres: [
-    'Action',
-    'Adventure',
-    'Family',
-    'Science Fiction'
-  ]
-}];
+import { MovieTile } from '../movie-tile';
+import { moviesData } from '../../../__mocks__/moviesData';
 
 describe('Movies component:', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(
-      <Movies moviesData={mockedMoviesData} />
+      <Movies moviesData={moviesData} />
     );
   });
 
@@ -36,21 +18,15 @@ describe('Movies component:', () => {
     expect(wrapper.find('.column')).toHaveLength(1);
   });
 
-  it('Should render one `MovieTile` without errors:', () => {
+  it('Should render `MovieTile` without errors:', () => {
     expect(wrapper.find(MovieTile)).toHaveLength(1);
   });
 
   describe('`MovieTile` component:', () => {
-    it('Should have prop `movieData`:', () => {
+    it('Should have the right value for prop `movieData`:', () => {
       const { movieData } = wrapper.find(MovieTile).props();
 
-      expect(movieData).toBeTruthy();
-    });
-
-    it('Prop `movieData` should be equal `mockedMoviesData`:', () => {
-      const { movieData } = wrapper.find(MovieTile).props();
-
-      expect(movieData).toEqual(mockedMoviesData[0]);
+      expect(movieData).toEqual(moviesData[0]);
     });
   });
 });
