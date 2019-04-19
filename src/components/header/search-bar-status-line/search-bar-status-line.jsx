dc65a118aaa5bc27048f0fdd523a6962c183f0cb
var-bin <var-bin@users.noreply.github.com>
2019-04-19 23:31:10 +0300

    First component with connection to redux store ;-)

--- src/components/header/search-bar-status-line/search-bar-status-line.jsx
+++ src/components/header/search-bar-status-line/search-bar-status-line.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import { setSortByFilter } from '../../../store/actions';
+
+import { SORT_BY } from '../../../constants';
 
 import './search-bar-status-line.styles.scss';
 
-export function SearchBarStatusLine() {
+function SearchBarStatusLineComponent(props) {
+  const {
+    setSortBy,
+    sortBy
+  } = props;
+
+  /* const [sortBy, setSortBy] = useState(SORT_BY.RELEASE_DATE);
+  const [moviesFound, setMoviesFound] = useState(0); */
+
+  const releaseDateClass = sortBy === SORT_BY.RELEASE_DATE
+    ? 'is-primary'
+    : 'is-light';
+
+  const ratingClass = sortBy === SORT_BY.RATING
+    ? 'is-primary'
+    : 'is-light';
+
   return (
     <div className="search-bar-status-line has-background-grey-lighter columns is-vcentered">
       <div className="column">
         <p>
-          7 movies found
+          {'0'}
+          &nbsp;
+          movies found
         </p>
       </div>
       <div className="column search-bar-status-line__">
         <div className="columns is-vcentered">
           <div className="column">Sort by: </div>
           <div className="column">
-            <button type="button" className="button is-primary">
+            <button type="button" className={`button ${releaseDateClass}`} onClick={() => setSortBy(SORT_BY.RELEASE_DATE)}>
               Release Date
             </button>
           </div>
           <div className="column">
-            <button type="button" className="button is-light">
+            <button type="button" className={`button ${ratingClass}`} onClick={() => setSortBy(SORT_BY.RATING)}>
               Rating
             </button>
           </div>
@@ -28,3 +51,16 @@ export function SearchBarStatusLine() {
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  sortBy: state.sortByFilter
+});
+
+const mapDispatchToProps = {
+  setSortBy: setSortByFilter
+};
+
+export const SearchBarStatusLine = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SearchBarStatusLineComponent);
