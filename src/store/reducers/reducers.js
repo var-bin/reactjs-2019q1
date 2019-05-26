c14e9986ed277265cba0448d819c6682732e93e0
var-bin <var-bin@users.noreply.github.com>
2019-05-26 12:38:59 +0300

    Fix sortByFilter reducer. Add sortBy feature

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -10,11 +10,35 @@ import {
   REQUEST_MOVIES
 } from 'app-store/action-types';
 
+const sortByVoteCount = (itemA, itemB) => {
+  if (itemB.vote_count > itemA.vote_count) {
+    return 1;
+  }
+
+  return -1;
+}
+
+const sortByReleaseDate = (itemA, itemB) => {
+  const releaseDateA = Number(itemA.release_date.replace(/-/g, ''));
+  const releaseDateB = Number(itemB.release_date.replace(/-/g, ''));
+
+  if (releaseDateB < releaseDateA) {
+    return -1;
+  }
+
+  return 1;
+}
 
 export function movies(state = [], action) {
   switch (action.type) {
     case RECEIVE_MOVIES:
       return action.payload.movies.data;
+    case SET_SORT_BY_FILTER:
+      const { filter } = action.payload;
+
+      return filter === SORT_BY.RELEASE_DATE
+        ? [...state].sort(sortByReleaseDate)
+        : [...state].sort(sortByVoteCount);
     default:
       return state;
   }
@@ -31,7 +55,7 @@ export function isFetching(state = false, action) {
   }
 }
 
-export function sortByFilter(state = SORT_BY.RELEASE_DATE, action) {
+export function sortByFilter(state = '', action) {
   switch (action.type) {
     case SET_SORT_BY_FILTER:
       return action.payload.filter;
