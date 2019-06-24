a86a2b986ec96fd09d869a94eac7c69e2b50036e
var-bin <var-bin@users.noreply.github.com>
2019-06-24 21:37:24 +0300

    Add aliases

--- jest.config.js
+++ jest.config.js
@@ -37,7 +37,11 @@ module.exports = {
 
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js'
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    'app-store': '<rootDir>/src/store',
+    'app-components': '<rootDir>/src/components',
+    'app-constants': '<rootDir>/src/constants',
+    'app-assets': '<rootDir>/src/assets'
   },
 
   moduleDirectories: [
