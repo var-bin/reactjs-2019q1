8af09822750dd81ea6a53ba81efa4b9309b65683
var-bin <var-bin@users.noreply.github.com>
2019-06-29 19:20:37 +0300

    Add new test cases

--- src/components/movie/components/movie-tile/movie-tile.test.jsx
+++ src/components/movie/components/movie-tile/movie-tile.test.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { MovieTile } from './movie-tile';
 import { moviesData } from 'app-mocks';
+
+import { MovieTile } from './movie-tile';
+
+import {
+  MovieRating
+} from '../movie-rating';
+
+import {
+  MovieReleaseDate
+} from '../movie-release-date';
+
+import {
+  MovieGenre,
+} from '../movie-genre';
 
 describe('MovieTile component:', () => {
   let wrapper;
@@ -21,5 +34,36 @@ describe('MovieTile component:', () => {
       .length;
 
     expect(movieGenres).toBeGreaterThan(0);
+  });
+
+  it('Should pass right props to `MovieGenre`:', () => {
+    wrapper.find('MovieGenres')
+      .dive()
+      .find(MovieGenre)
+      .map((movieGenre, movieGenreIndex) => {
+        const { genre } = movieGenre.props();
+
+        expect(genre).toEqual(moviesData[0].genres[movieGenreIndex]);
+      });
+  });
+
+  it('Should render `MovieRating` without errors:', () => {
+    expect(wrapper.find(MovieRating)).toHaveLength(1);
+  });
+
+  it('Should pass right props to `MovieRating`:', () => {
+    const { rating } = wrapper.find(MovieRating).props();
+
+    expect(rating).toEqual(moviesData[0].vote_count);
+  });
+
+  it('Should render `MovieReleaseDate` without errors:', () => {
+    expect(wrapper.find(MovieReleaseDate)).toHaveLength(1);
+  });
+
+  it('Should pass right props to `MovieReleaseDate`:', () => {
+    const { releaseDate } = wrapper.find(MovieReleaseDate).props();
+
+    expect(releaseDate).toEqual(moviesData[0].release_date);
   });
 });
