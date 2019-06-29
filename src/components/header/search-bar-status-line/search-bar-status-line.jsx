01782ca14bd375be5b2de741c36c8aa5ea188313
var-bin <var-bin@users.noreply.github.com>
2019-06-29 17:15:06 +0300

    Add movies found feature.

--- src/components/header/search-bar-status-line/search-bar-status-line.jsx
+++ src/components/header/search-bar-status-line/search-bar-status-line.jsx
@@ -4,9 +4,14 @@ import { connect } from 'react-redux';
 import { SORT_BY } from 'app-constants';
 
 import {
+  getMovies
+} from 'app-components/movie/components/movie-list/store';
+
+import {
   setSortBy,
 
-  getSortByFilter
+  getSortByFilter,
+  getSearchMoviesValue
 } from './store';
 
 import './search-bar-status-line.styles.scss';
@@ -14,7 +19,9 @@ import './search-bar-status-line.styles.scss';
 export function SearchBarStatusLineComponent(props) {
   const {
     setSortByFilter,
-    sortBy
+    sortBy,
+    foundMovies,
+    searchMoviesValue
   } = props;
 
   const releaseDateClass = sortBy === SORT_BY.RELEASE_DATE
@@ -25,15 +32,41 @@ export function SearchBarStatusLineComponent(props) {
     ? 'is-primary'
     : 'is-light';
 
+  const isSearchMoviesValueExisted = searchMoviesValue !== '';
+
+  const getMoviesFound = () => {
+    if (isSearchMoviesValueExisted) {
+      return foundMovies.length;
+    }
+  }
+
+  const movieMoviesWords = () => {
+    if (getMoviesFound() > 1) {
+      return 'movies found';
+    }
+
+    return 'movie found';
+  }
+
+  const renderfoundMoviesInfo = () => {
+    if (isSearchMoviesValueExisted) {
+      return (
+        <div className="column">
+          <p>
+            {getMoviesFound()}
+            &nbsp;
+            {movieMoviesWords()}
+          </p>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
   return (
     <div className="search-bar-status-line has-background-grey-lighter columns is-vcentered">
-      <div className="column">
-        <p>
-          {'0'}
-          &nbsp;
-          movies found
-        </p>
-      </div>
+      {renderfoundMoviesInfo()}
       <div className="column search-bar-status-line__sort-by">
         <div className="columns is-vcentered">
           <div className="column is-narrow search-bar-status-line__sort-by-title">Sort by: </div>
@@ -62,7 +95,9 @@ export function SearchBarStatusLineComponent(props) {
 }
 
 const mapStateToProps = state => ({
-  sortBy: getSortByFilter(state)
+  sortBy: getSortByFilter(state),
+  searchMoviesValue: getSearchMoviesValue(state),
+  foundMovies: getMovies(state)
 });
 
 const mapDispatchToProps = {
