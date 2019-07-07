c437b7f4809011e1bfbbeb174c8e83ca06bb1c19
var-bin <var-bin@users.noreply.github.com>
2019-07-07 22:41:25 +0300

    Fix paths

--- buildTools/paths.js
+++ buildTools/paths.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  DIST: path.resolve(__dirname, '../dist'),
+  DIST: path.resolve(__dirname, '..', 'dist'),
   ENTRY: './index.js',
   PUBLIC_PATH: '/',
   FILENAME: '[name].bundle.js',
   HTML_TEMPLATE: './index.html',
   FAVICON: './favicon.ico',
-  APP_COMPONENTS: path.resolve(__dirname, '../src/components'),
-  APP_STORE: path.resolve(__dirname, '../src/store'),
-  APP_CONSTANTS: path.resolve(__dirname, '../src/constants'),
-  APP_ASSETS: path.resolve(__dirname, '../src/assets'),
-  MOVIE_DATA_MOCK: path.resolve(__dirname, '../__mocks__/moviesData')
+  APP_COMPONENTS: path.resolve(__dirname, '..', 'src', 'components'),
+  APP_STORE: path.resolve(__dirname, '..', 'src', 'store'),
+  APP_CONSTANTS: path.resolve(__dirname, '..', 'src', 'constants'),
+  APP_ASSETS: path.resolve(__dirname, '..', 'src', 'assets'),
+  MOVIE_DATA_MOCK: path.resolve(__dirname, '..', 'src', '__mocks__', 'moviesData')
 };
