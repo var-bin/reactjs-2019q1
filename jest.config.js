c6dd440e87bcda5c65f5b67de3e84b175df15ab1
var-bin <var-bin@users.noreply.github.com>
2019-07-07 22:41:48 +0300

    Fix aliases regexp for jest

--- jest.config.js
+++ jest.config.js
@@ -39,23 +39,30 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist',
+    '<rootDir>/coverage',
+    '<rootDir>/cypress',
+    '<rootDir>/.cache'
+  ],
+
   // Indicates whether each individual test should be reported during the run
   verbose: false,
 
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    'app-store': '<rootDir>/src/store',
-    'app-components': '<rootDir>/src/components',
-    'app-constants': '<rootDir>/src/constants',
-    'app-assets': '<rootDir>/src/assets',
-    'app-mocks': '<rootDir>/__mocks__'
+
+    // '^app-store(.*)$' is matched with any path started with app-store...
+    '^app-components(.*)$': '<rootDir>/src/components$1',
+    '^app-constants(.*)$': '<rootDir>/src/constants$1',
+    '^app-store(.*)$': '<rootDir>/src/store$1',
+    '^app-assets(.*)$': '<rootDir>/src/assets$1',
+    '^app-mocks(.*)$': '<rootDir>/__mocks__$1'
   },
 
   moduleDirectories: [
     'node_modules',
     'src'
-  ],
-
-  setupFilesAfterEnv: ['<rootDir>/jest-extended.config.js']
+  ]
 };
