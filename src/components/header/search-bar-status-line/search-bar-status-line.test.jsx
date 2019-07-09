fccf538e3ba8cd132224cfdb75cd8bcac18ff714
var-bin <var-bin@users.noreply.github.com>
2019-07-09 22:45:28 +0300

    Add new tests

--- src/components/header/search-bar-status-line/search-bar-status-line.test.jsx
+++ src/components/header/search-bar-status-line/search-bar-status-line.test.jsx
@@ -28,7 +28,41 @@ describe('SearchBarStatusLineComponent component:', () => {
     wrapper = generateWrapper();
   });
 
-  it('Should render without errors:', () => {
-    expect(wrapper.find('.search-bar-status-line')).toHaveLength(1);
+  describe('Default behavior', () => {
+    it('should render without errors:', () => {
+      expect(wrapper.find('.search-bar-status-line')).toHaveLength(1);
+    });
+
+    it('should not render info related to searching result:', () => {
+      expect(wrapper.find('[data-ut-id="foundMoviesInfo"]')).toHaveLength(0);
+    });
+  });
+
+  describe('Info related to searching result', () => {
+    it('should render rigth text for more than one movie: ', () => {
+      const expected = '2 movies found';
+
+      wrapper = generateWrapper({
+        searchMoviesValue: 'test',
+        foundMovies: ['movie 1', 'movie 2']
+      });
+
+      const foundMoviesInfo = wrapper.find('[data-ut-id="foundMoviesInfo"]').text();
+
+      expect(foundMoviesInfo).toEqual(expected);
+    });
+
+    it('should render rigth text for one movie: ', () => {
+      const expected = '1 movie found';
+
+      wrapper = generateWrapper({
+        searchMoviesValue: 'test',
+        foundMovies: ['movie 1']
+      });
+
+      const foundMoviesInfo = wrapper.find('[data-ut-id="foundMoviesInfo"]').text();
+
+      expect(foundMoviesInfo).toEqual(expected);
+    });
   });
 });
