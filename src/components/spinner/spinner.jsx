854b152036899ab542e6ecc09347f7b08f7a9e7d
var-bin <var-bin@users.noreply.github.com>
2019-07-10 21:56:26 +0300

    Add data attribute for test needs

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -6,7 +6,7 @@ export function Spinner() {
   return (
     <div className="spinner-wrapper">
       <span className="icon is-large">
-        <i className="fas fa-spinner fa-pulse" />
+        <i className="fas fa-spinner fa-pulse" data-ut-id="spinnerIcon" />
       </span>
     </div>
   );
