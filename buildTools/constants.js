78962c88117b8d37bdabbfaff0d1fd1b9483ba2a
var-bin <var-bin@users.noreply.github.com>
2019-07-16 12:01:06 +0300

    Add APP_TITLE

--- buildTools/constants.js
+++ buildTools/constants.js
@@ -5,5 +5,6 @@ module.exports = {
   DEVTOOL_PRODUCTION: 'source-map',
   HTML_TITLE_DEVELOPMENT_MODE: 'Development',
   HTML_TITLE_PRODUCTION_MODE: 'Production',
-  NODE_ENV: 'process.env.NODE_ENV'
+  NODE_ENV: 'process.env.NODE_ENV',
+  APP_TITLE: '🎥 Movie app'
 };
