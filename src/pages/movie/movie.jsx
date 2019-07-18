849cd0890795deaf006f8f5ae07f80a5030f4e99
var-bin <var-bin@users.noreply.github.com>
2019-07-18 22:58:05 +0300

    Add prop

--- src/pages/movie/movie.jsx
+++ src/pages/movie/movie.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 import { MovieDetailsComponent } from 'app-components/movie';
 
-export function MovieComponent() {
+export function MovieComponent({ match }) {
+  const {
+    params
+  } = match;
+
+  console.log('MovieComponent: ', match, params);
+
   return (
     <div className="section">
       <div className="section">
