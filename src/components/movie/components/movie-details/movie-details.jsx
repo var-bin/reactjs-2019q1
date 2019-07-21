1d15b8598f4205419cf48acdf185a2c7bc254495
var-bin <var-bin@users.noreply.github.com>
2019-07-21 20:19:02 +0300

    Edit markup for MovieDetailsComponent

--- src/components/movie/components/movie-details/movie-details.jsx
+++ src/components/movie/components/movie-details/movie-details.jsx
@@ -1,10 +1,61 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-export function MovieDetailsComponent() {
+export function MovieDetailsComponent(props) {
+  const {
+    movieData: {
+      title,
+      tagline,
+      release_date: releaseDate,
+      runtime,
+      overview,
+      poster_path: imgPath
+    }
+  } = props;
+
   return (
-    <div className="section">
-      <div className="container">
-        <h2 className="title">Movie details page</h2>
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <Link to="/">
+            Back to search
+          </Link>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <figure className="image is-2by3">
+            <span className="icon is-large">
+              <i className="fas fa-spinner fa-pulse" />
+            </span>
+            <img src={imgPath} alt="Movie Poster" />
+          </figure>
+        </div>
+        <div className="column is-three-quarters">
+          <div className="columns is-multiline">
+            <div className="column is-12">
+              <h3 className="title">
+                {title}
+              </h3>
+            </div>
+            <div className="column is-12">
+              {tagline}
+            </div>
+            <div className="column is-12">
+              <div className="columns">
+                <div className="column is-2">
+                  {releaseDate}
+                </div>
+                <div className="column is-2">
+                  {runtime} min
+                </div>
+              </div>
+            </div>
+            <div className="column is-12">
+              {overview}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
